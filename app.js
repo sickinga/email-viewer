1c00be252d00625f65c86e23da79a58affacc831
Thomas Sickinger <sicki333@gmail.com>
2022-11-28 13:19:29 +0100

    restructure + initial mail pull

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const client = new Client({
 const hostname = "127.0.0.1";
 const port = 8000;
 let mail = "";
+let lastMailTimestamp = 0;
+
 const server = http.createServer(async (req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
@@ -32,27 +34,27 @@ server.on("close", client.quit);
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-    startMailPulling();
+    pullMails();
+    setInterval(pullMails, 30000);
 });
 
 server.on("close", () => client.quit());
 
-function startMailPulling() {
-    console.log("Connected to mail server");
-    let lastMailTimestamp = 0;
-    setInterval(() => {
-        client.connect().then(() =>
-            client.retrieveAll().then((messages) => {
-                try {
-                    const newMail = messages.sort(
-                        (a, b) =>
-                            b.receivedDate.getTime() - a.receivedDate.getTime()
-                    )[0];
-                    if (lastMailTimestamp < newMail.receivedDate.getTime()) {
-                        console.log("Messages retrieved");
-                        lastMailTimestamp = newMail.receivedDate.getTime();
-                        mail = newMail.html;
-                        exec("xset s reset && xset dpms force on", (error, stdout, stderr) => {
+function pullMails() {
+    client.connect().then(() =>
+        client.retrieveAll().then((messages) => {
+            try {
+                const newMail = messages.sort(
+                    (a, b) =>
+                        b.receivedDate.getTime() - a.receivedDate.getTime()
+                )[0];
+                if (lastMailTimestamp < newMail.receivedDate.getTime()) {
+                    console.log("Messages retrieved");
+                    lastMailTimestamp = newMail.receivedDate.getTime();
+                    mail = newMail.html;
+                    exec(
+                        "xset s reset && xset dpms force on",
+                        (error, stdout, stderr) => {
                             if (error) {
                                 console.log(`error: ${error.message}`);
                                 return;
@@ -62,14 +64,14 @@ function startMailPulling() {
                                 return;
                             }
                             console.log(`stdout: ${stdout}`);
-                        });
-                    }
-                    client.quit();
-                } catch (e) {
-                    client.quit();
-                    console.log(e);
+                        }
+                    );
                 }
-            })
-        );
-    }, 30000);
+                client.quit();
+            } catch (e) {
+                client.quit();
+                console.log(e);
+            }
+        })
+    );
 }
